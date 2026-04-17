96a40dc5df5bb9e50e014b7dc5dcf40341137c64
Saebyul Hong <104213415+StarshineStudios@users.noreply.github.com>
2026-04-16 21:50:50 -0400

    setup

---
+++ multiplayer-system-3d/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+
+
+func _on_host_game_pressed() -> void:
+	NetworkManager.create_server()
+	
+func _on_join_game_pressed() -> void:
+	NetworkManager.create_client()
+	
+func _on_send_test_message_pressed() -> void:
+	_send_test_message.rpc("I am connected to you")
+
+@rpc("any_peer", "call_remote", )
+func _send_test_message(message: String):
+	print("Peer [%s] recieved message [%s] from peer [%s]" 
+	%[get_tree().get_multiplayer().get_unique_id(), 
+	message,
+	get_tree().get_multiplayer().get_remote_sender_id()])
+	
