e777174e036de9fe43a090fa189039860129157e
Saebyul Hong <104213415+StarshineStudios@users.noreply.github.com>
2026-04-19 19:53:23 -0400

    make projectile spawner have a tool script

--- multiplayer-system-3d/player/auto_projectile_spawner.gd
+++ multiplayer-system-3d/player/auto_projectile_spawner.gd
@@ -1,11 +1,36 @@
+@tool
 extends MultiplayerSpawner
 
-func _ready() -> void:
-	var dir = DirAccess.open("res://weapon/projectiles/scenes/")
-	if dir:
-		dir.list_dir_begin()
-		var file = dir.get_next()
-		while file != "":
-			if file.ends_with(".tscn"):
-				add_spawnable_scene("res://weapon/projectiles/scenes/" + file)
-			file = dir.get_next()
+@export var scan_projectiles: bool:
+	set(value):
+		if value:
+			_scan_and_register()
+			scan_projectiles = false
+
+var _registered := {}
+
+func _scan_and_register() -> void:
+	var path := "res://weapon/projectiles/scenes/"
+	var dir := DirAccess.open(path)
+	
+	if not dir:
+		push_error("Failed to open directory: " + path)
+		return
+
+	dir.list_dir_begin()
+	var file := dir.get_next()
+
+	while file != "":
+		if not dir.current_is_dir() and file.ends_with(".tscn"):
+			var full_path := path + file
+			
+			if not _registered.has(full_path):
+				add_spawnable_scene(full_path)
+				_registered[full_path] = true
+		
+		file = dir.get_next()
+
+	dir.list_dir_end()
+
+	if Engine.is_editor_hint():
+		notify_property_list_changed()
